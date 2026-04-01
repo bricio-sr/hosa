f1e866c122f87542a89aa9b3d5877ddd30660de1
github-action-benchmark <github@users.noreply.github.com>
2026-04-01 12:53:31 +0000

    add Benchmark (go) benchmark result for 2013d371f899c847b6f216c92a15b031a66edd59

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1774624794756,
+  "lastUpdate": 1775048011352,
   "repoUrl": "https://github.com/bricio-sr/hosa",
   "entries": {
     "Benchmark": [
@@ -300,6 +300,306 @@ window.BENCHMARK_DATA = {
             "value": 0,
             "unit": "allocs/op",
             "extra": "183485660 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "f@bricio.dev.br",
+            "name": "Fabricio Amorim",
+            "username": "bricio-sr"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2013d371f899c847b6f216c92a15b031a66edd59",
+          "message": "feat(docs): Whitepaper v2.2",
+          "timestamp": "2026-04-01T09:52:37-03:00",
+          "tree_id": "108e843d82b4f97061c204afb92cb2e61581d7f1",
+          "url": "https://github.com/bricio-sr/hosa/commit/2013d371f899c847b6f216c92a15b031a66edd59"
+        },
+        "date": 1775048010970,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkAnalyzeCycle",
+            "value": 3423,
+            "unit": "ns/op\t   10224 B/op\t       6 allocs/op",
+            "extra": "1779700 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeCycle - ns/op",
+            "value": 3423,
+            "unit": "ns/op",
+            "extra": "1779700 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeCycle - B/op",
+            "value": 10224,
+            "unit": "B/op",
+            "extra": "1779700 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeCycle - allocs/op",
+            "value": 6,
+            "unit": "allocs/op",
+            "extra": "1779700 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution",
+            "value": 0.03544,
+            "unit": "ns/op\t      2384 p50_ns\t     72595 p999_ns\t     25157 p99_ns\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution - ns/op",
+            "value": 0.03544,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution - p50_ns",
+            "value": 2384,
+            "unit": "p50_ns",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution - p999_ns",
+            "value": 72595,
+            "unit": "p999_ns",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution - p99_ns",
+            "value": 25157,
+            "unit": "p99_ns",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAnalyzeLatencyDistribution - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWelfordUpdate",
+            "value": 57.84,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "100000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWelfordUpdate - ns/op",
+            "value": 57.84,
+            "unit": "ns/op",
+            "extra": "100000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWelfordUpdate - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "100000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkWelfordUpdate - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "100000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMahalanobisCalculation",
+            "value": 148.6,
+            "unit": "ns/op\t     136 B/op\t       4 allocs/op",
+            "extra": "39876280 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMahalanobisCalculation - ns/op",
+            "value": 148.6,
+            "unit": "ns/op",
+            "extra": "39876280 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMahalanobisCalculation - B/op",
+            "value": 136,
+            "unit": "B/op",
+            "extra": "39876280 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkMahalanobisCalculation - allocs/op",
+            "value": 4,
+            "unit": "allocs/op",
+            "extra": "39876280 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate",
+            "value": 0.04141,
+            "unit": "ns/op\t      1821 false_positives\t        18.21 fpr_%\t     10000 total_cycles\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate - ns/op",
+            "value": 0.04141,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate - false_positives",
+            "value": 1821,
+            "unit": "false_positives",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate - fpr_%",
+            "value": 18.21,
+            "unit": "fpr_%",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate - total_cycles",
+            "value": 10000,
+            "unit": "total_cycles",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkFalsePositiveRate - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak",
+            "value": 0.008656,
+            "unit": "ns/op\t         1.000 cycles_to_detect\t        50.00 leak_rate_per_cycle\t      1000 ms_to_detect\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak - ns/op",
+            "value": 0.008656,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak - cycles_to_detect",
+            "value": 1,
+            "unit": "cycles_to_detect",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak - leak_rate_per_cycle",
+            "value": 50,
+            "unit": "leak_rate_per_cycle",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak - ms_to_detect",
+            "value": 1000,
+            "unit": "ms_to_detect",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_MemoryLeak - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_CPUBurn",
+            "value": 0.007584,
+            "unit": "ns/op\t       200.0 cycles_after_fault\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_CPUBurn - ns/op",
+            "value": 0.007584,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_CPUBurn - cycles_after_fault",
+            "value": 200,
+            "unit": "cycles_after_fault",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_CPUBurn - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkDetectionRate_CPUBurn - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocationsPerCycle",
+            "value": 4097,
+            "unit": "ns/op\t   10600 B/op\t      12 allocs/op",
+            "extra": "1462198 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocationsPerCycle - ns/op",
+            "value": 4097,
+            "unit": "ns/op",
+            "extra": "1462198 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocationsPerCycle - B/op",
+            "value": 10600,
+            "unit": "B/op",
+            "extra": "1462198 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkAllocationsPerCycle - allocs/op",
+            "value": 12,
+            "unit": "allocs/op",
+            "extra": "1462198 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRingBufferInsert",
+            "value": 16.56,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "362584306 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRingBufferInsert - ns/op",
+            "value": 16.56,
+            "unit": "ns/op",
+            "extra": "362584306 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRingBufferInsert - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "362584306 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkRingBufferInsert - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "362584306 times\n4 procs"
           }
         ]
       }
